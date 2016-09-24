fe9c5a453276144c9265e9975df8ae02f53fd3f8
Allan Arnesen <allan.arnesen@gmail.com>
2016-09-24 13:03:46 +0200

    Added task4 - EvaluateExpression.

---
+++ Oblig_1_H2016/test/task4/EvaluateExpressiontest.java
@@ -0,0 +1,14 @@
+package task4;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EvaluateExpressiontest {
+
+	@Test
+	public void test() {
+		System.out.println(EvaluateExpression.evaluateExpression("1 2 + 3 *"));
+	}
+
+}
