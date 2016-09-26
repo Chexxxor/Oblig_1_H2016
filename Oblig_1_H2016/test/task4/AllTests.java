cea1affef736f696f37a3deb7899d071dfe76c35
Allan Arnesen <allan.arnesen@gmail.com>
2016-09-26 18:32:56 +0200

    Added test for Exceptions and a test Suite

---
+++ Oblig_1_H2016/test/task4/AllTests.java
@@ -0,0 +1,12 @@
+package task4;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ EvaluateExpressiontest.class,
+		EvaluateExpressiontestExceptions.class })
+public class AllTests {
+
+}
